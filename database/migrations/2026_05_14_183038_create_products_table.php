968e3d7f7c95c5c34ce90678487e088a3a70e763
hammakara <hammakara51@gmail.com>
2026-05-22 17:12:46 +0700

    Update products migration to match API and frontend

--- database/migrations/2026_05_14_183038_create_products_table.php
+++ database/migrations/2026_05_14_183038_create_products_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-             $table->string('title');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
             $table->text('description')->nullable();
-            $table->string("banner_image",220)->nullable();
-            $table->decimal('cost')->nullable();
-            $table->foreignId('user_id')->constrained("users")->cascadeOnDelete();
+            $table->enum('status', ['active', 'inactive'])
+                  ->default('active');
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->cascadeOnDelete();
             $table->timestamps();
         });
     }
